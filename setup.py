c98078b22c1f6e37748fcbaa8a08787792e0168f
Whit Morriss <whit@surveymonkey.com>
2011-03-17 01:53:26 -0700

    remove tests

--- setup.py
+++ setup.py
@@ -6,9 +6,17 @@ version = '0.1'
 setup(name='pastehttp/',
       version=version,
       description="The Paste HTTP WSGI Server",
-      long_description="""\
-""",
-      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      long_description=open('README.rst').read(),
+      classifiers=["Development Status :: 5 - Production/Stable",
+                   "Intended Audience :: Developers",
+                   "License :: OSI Approved :: MIT License",
+                   "Programming Language :: Python",
+                   "Topic :: Internet :: WWW/HTTP",
+                   "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+                   "Topic :: Software Development :: Libraries :: Python Modules",
+                   "Topic :: Internet :: WWW/HTTP :: WSGI",
+                   "Topic :: Internet :: WWW/HTTP :: WSGI :: Server",
+    "Framework :: Paste"], 
       keywords='',
       author='Ian Bicking',
       author_email='whit at surveymonkey.com',
@@ -21,6 +29,10 @@ setup(name='pastehttp/',
           # -*- Extra requirements: -*-
       ],
       entry_points="""
-      # -*- Entry points: -*-
+      [paste.server_runner]
+      http = paste.httpserver:server_runner
+
+      [karnac.server_runner]
+      http = paste.httpserver:server_runner      
       """,
       )
